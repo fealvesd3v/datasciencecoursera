110e3e171ef0eb105224e436e6779c4a0de273e1
fealvesd3v <falves@d3v.solutions>
2015-04-26 13:36:22 -0400

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,69 @@
+#This first function, makeVector creates a special "vector", which is really a list containing a function to
+#   Set the value of the vector
+#   Get the value of the vector
+#   Set the value of the mean
+#   Get the value of the mean
+makeCacheMatrix <- function(matrix = matrix()) {
+  
+   ##Initializes the cache to null
+   cache <- NULL
+
+   ##Set matrix 
+   set <- function(newMatrix) {
+      ##Assign newMatrix to matrix
+      matrix <<- newMatrix
+      ##Clear Cache
+      cache <<- NULL
+   }
+
+  ##Retrieve matrix
+  get <- function() {
+     ##Returns the stored matrix
+     matrix
+  }
+  
+  ##Set inverse matrix
+  setInverse <- function(inverseMatrix){
+     ##Inverse matrix is assigned to cache
+     cache <<- inverseMatrix
+  }
+  
+  ##Retrieve the inverse matrix
+  getInverse <- function() {
+     ##Returns the cached inverse matrix
+     cache
+  }
+  
+  ##Returns a list with references to all the functions defined above.
+  list(
+         set = set, get = get,
+         setInverse = setInverse, getInverse = getInverse
+  )
+}
+
+##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated and cached (and the matrix has not changed), 
+##then the cachesolve will retrieve the inverse from the cache.
+cacheSolve <- function(matrix, ...) {
+   
+   ## Retrieves the inverse matrix and assigns it to the cache
+   cache <- matrix$getInverse()
+
+   ##Checks if the cache is null 
+   if(!is.null(cache)) {
+      ##If so, issue message
+      message("Retrieving cached matrix...")
+      ##Force "premature" return of the function
+      ##Return the cached matrix
+      return(cache)
+  }
+  
+  ##Retrieves the stored matrix and assigns it to data
+  data <- matrix$get()
+  ##Calculates the inverse matrix
+  cache <- solve(data, ...)
+  ##Sets the inverse matrix
+  matrix$setInverse(cache)
+  ##Returns the cached inverted matrix
+  cache
+}
